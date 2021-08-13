a6ed6f2df0bfcc66d54305db2660aac6c758179c
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2021-08-13 15:45:38 +0300

    projectMesto/2

--- script/index.js
+++ script/index.js
@@ -3,26 +3,28 @@ let close = document.querySelector('.popup__cross');
 let pop =  document.querySelector('.popup');
 let nameInput = document.querySelector('#name');
 let profInput = document.querySelector('#prof');
+let profileName = document.querySelector('.profile__name');
+let profileProf = document.querySelector('.profile__prof');
 
-
-
-editButton.addEventListener('click', function() {
+function openPopup() {
   pop.classList.add('popup_open');
-  nameInput.value = document.querySelector('.profile__name').innerHTML;
-  profInput.value = document.querySelector('.profile__prof').innerHTML;
-});
-close.addEventListener('click', function() {
+  nameInput.value = profileName.textContent;
+  profInput.value = profileProf.textContent;
+}
+function closePopup () {
   pop.classList.remove('popup_open');
-});
+}
+editButton.addEventListener('click', openPopup);
+close.addEventListener('click',closePopup);
 
-
-let formElement = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
 function formSubmitHandler (evt) {
 	evt.preventDefault();
 
-	 document.querySelector('.profile__name').textContent = nameInput.value;
-	 document.querySelector('.profile__prof').textContent = profInput.value;
-   pop.classList.remove('popup_open');
+	 profileName.textContent = nameInput.value;
+	 profileProf.textContent = profInput.value;
+
+   closePopup ();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
